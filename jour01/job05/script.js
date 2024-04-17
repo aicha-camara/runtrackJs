da69f32d5c7bba9b9508e4e891349dd67eddc8df
camara_aicha <aicha.ca@outlook.fr>
2024-04-17 09:51:07 +0200

    ajout de la fonction afficherJoursSemaines qui recupere les valeurs de ma liste et qui l'affiche dans la console

---
+++ jour01/job05/script.js
@@ -0,0 +1,6 @@
+function afficherJoursSemaines(){
+            var tableau = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+            for (var i = 0; i < tableau.length; i++) {
+        console.log(tableau[i]);
+    }
+}
